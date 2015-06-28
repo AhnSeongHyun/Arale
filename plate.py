64a862a98cfc0702f4593fc481874d5590fdb9f7
AhnSeongHyun <sh84.ahn@gmail.com>
2015-06-28 14:42:27 +0900

    addplate.py

---
+++ plate.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+__author__ = 'sh84.ahn@gmail.com'
+
+from plate_base import *
+
+import api
+import views
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ plate_base.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+__author__ = 'sh84.ahn@gmail.com'
+
+import traceback
+
+from flask import Flask
+from flask import redirect
+from flask import url_for
+from flask import session
+from flask import request
+from flask import make_response
+from flask import jsonify
+from flask import render_template
+
+
+app = Flask(__name__, static_url_path = "", static_folder="bower_components")
+app.debug = True
+
+#HTTP_METHOD
+GET = 'GET'
+POST = 'POST'
+PUT = 'PUT'
+DELETE = 'DELETE'
+HEAD = 'HEAD'
